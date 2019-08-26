2fc38e2819699e166c2f88a55fc9363df25cc3a9
Skyler <svaldez85@unm.edu>
2019-08-26 12:19:26 -0600

    commit 2

--- hello.cpp
+++ hello.cpp
@@ -2,32 +2,33 @@
 
 int main()
 {
-	std::cout << "##Introduction to Skyler Valdez  ";
-	std::cout << "**A Collection of Dumb Haikus**  ";
-	std::cout << "1.  ";
-	std::cout << "I am a student.  ";
-	std::cout << "I cry tears every day  ";
-	std::cout << "Engineering **hurts**  ";
+	std::cout << "## Introduction to Skyler Valdez" << std::endl;
+	std::cout << "**A Collection of Dumb Haikus**" << std::endl;
+	std::cout << "1." << std::endl;
+	std::cout << "I am a student." << std::endl;
+	std::cout << "I cry tears every day" << std::endl;
+	std::cout << "Engineering **hurts**" << std::endl;
 	
-	std::cout << "2.  ";
-	std::cout << "I have a dog.  ";
-	std::cout << "He is my son and mentor.  ";
-	std::cout << "*See attached image*.  ";
+	std::cout << "2." << std::endl;
+	std::cout << "I have a dog." << std::endl;
+	std::cout << "He is my son and mentor." << std::endl;
+	std::cout << "*See attached image*." << std::endl;
+	std::cout << "![my son] (yogurt.jpg)" << std::endl;
 	
-	std::cout << "3.  ";
-	std::cout << "I paint sometimes, too,  ";
-	std::cout << "but am poor; so if I paint  ";
-	std::cout << "I'd like to be paid.  ";
+	std::cout << "3." << std::endl;
+	std::cout << "I paint sometimes, too," << std::endl;
+	std::cout << "but am poor; so if I paint" << std::endl;
+	std::cout << "I'd like to be paid.  " << std::endl;
 	
-	std::cout << "4.  ";
-	std::cout << "On the subject of  ";
-	std::cout << "Poverty, please petition  ";
-	std::cout << "For more free textbooks.  ";
+	std::cout << "4.  " << std::endl;
+	std::cout << "On the subject of  " << std::endl;
+	std::cout << "Poverty, please petition  " << std::endl;
+	std::cout << "For more free textbooks.  " << std::endl;
 	
-	std::cout << "5.  ";
-	std::cout << "Can you petition  ";
-	std::cout << "for that? Why haven't we yet? ";
-	std::cout << "We need a union.";
+	std::cout << "5.  " << std::endl;
+	std::cout << "Can you petition  " << std::endl;
+	std::cout << "for that? Why haven't we yet? " << std::endl;
+	std::cout << "We need a union." << std::endl;
 
 
 }
