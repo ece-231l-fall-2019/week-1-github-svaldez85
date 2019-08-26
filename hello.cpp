60d98fcf6388db9c1c14a0094ec65e5da40e0d9a
Skyler <svaldez85@unm.edu>
2019-08-26 12:28:30 -0600

    Attempting to add jpeg

--- hello.cpp
+++ hello.cpp
@@ -2,22 +2,22 @@
 
 int main()
 {
-	std::cout << "## Introduction to Skyler Valdez" << std::endl;
-	std::cout << "**A Collection of Dumb Haikus**" << std::endl;
-	std::cout << "1." << std::endl;
-	std::cout << "I am a student." << std::endl;
-	std::cout << "I cry tears every day" << std::endl;
-	std::cout << "Engineering **hurts**" << std::endl;
+	std::cout << "## Introduction to Skyler Valdez  " << std::endl;
+	std::cout << "**A Collection of Dumb Haikus**  " << std::endl;
+	std::cout << "1.  " << std::endl;
+	std::cout << "I am a student.  " << std::endl;
+	std::cout << "I cry tears every day  " << std::endl;
+	std::cout << "Engineering **hurts**  " << std::endl;
 	
-	std::cout << "2." << std::endl;
-	std::cout << "I have a dog." << std::endl;
-	std::cout << "He is my son and mentor." << std::endl;
-	std::cout << "*See attached image*." << std::endl;
-	std::cout << "![my son] (yogurt.jpg)" << std::endl;
+	std::cout << "2.  " << std::endl;
+	std::cout << "I have a dog.  " << std::endl;
+	std::cout << "He is my son and mentor.  " << std::endl;
+	std::cout << "*See attached image*.  " << std::endl;
+	std::cout << "![my son](yogurt.jpg)  " << std::endl;
 	
-	std::cout << "3." << std::endl;
-	std::cout << "I paint sometimes, too," << std::endl;
-	std::cout << "but am poor; so if I paint" << std::endl;
+	std::cout << "3.  " << std::endl;
+	std::cout << "I paint sometimes, too,  " << std::endl;
+	std::cout << "but am poor; so if I paint  " << std::endl;
 	std::cout << "I'd like to be paid.  " << std::endl;
 	
 	std::cout << "4.  " << std::endl;
@@ -27,8 +27,8 @@ int main()
 	
 	std::cout << "5.  " << std::endl;
 	std::cout << "Can you petition  " << std::endl;
-	std::cout << "for that? Why haven't we yet? " << std::endl;
-	std::cout << "We need a union." << std::endl;
+	std::cout << "for that? Why haven't we yet?  " << std::endl;
+	std::cout << "We need a union.  " << std::endl;
 
 
 }
